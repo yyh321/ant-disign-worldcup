a4049b831fa1e65cf247a40bb0994c0cbaab785d
yyh123 <yunhaoyu@gmail.com>
2019-01-09 04:23:17 +0800

    request data

--- src/App.js
+++ src/App.js
@@ -1,41 +1,69 @@
 import React, { Component } from 'react';
 // import Button from "antd/lib/button";
 import { Checkbox,Radio,Table  } from "antd";
+import reqwest from 'reqwest';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 // import {}
 const RadioButton = Radio.Button;
 const RadioGroup = Radio.Group;
 
-const dataSource = [{
-  key: '1',
-  name: '胡彦斌',
-  age: 32,
-  address: '西湖区湖底公园1号'
-}, {
-  key: '2',
-  name: '胡彦祖',
-  age: 42,
-  address: '西湖区湖底公园1号'
-}];
-
-const columns = [{
-  title: '姓名',
-  dataIndex: 'name',
-  key: 'name',
-}, {
-  title: '年龄',
-  dataIndex: 'age',
-  key: 'age',
-}, {
-  title: '住址',
-  dataIndex: 'address',
-  key: 'address',
-}];
-
+const columns = [
+  {
+  title:'赛事',
+  dataIndex:'league[0]'
+  },
+  {
+  title:'时间',
+  dataIndex:'matchTime',
+  render:(value, record) => <span title={record.matchYear + '-' + record.matchDate + ' ' + record.matchTime}> {record.matchDate + ' ' + record.matchTime}</span>
+  },
+  {
+  title:'主队',
+  dataIndex:'home[0]'
+  },
+  {
+    title:'全场比分',
+    dataIndex:'score',
+    render:(value, record) => <span> {record.homeScore} - {record.guestScore} </span>
+  },
+  {
+    title:'客队',
+    dataIndex:'guest[0]'
+  },
+  {
+    title:'半场比分',
+    dataIndex:'halfScore',
+    render:(value, record) => <span> {record.homeHalfScore} - {record.guestHalfScore} </span>
+  },
+];
 
 class App extends Component {
-  
+  state = {
+    data: [],
+    loading:false
+  };
+
+  fetch = () => {
+    this.setState({ loading: true });
+    this.setState({loading:true});
+    reqwest({
+      url: 'worldcup_2018.json',
+      method: 'get',
+      type: 'json',
+    }).then((data) => {
+      // Read total count from server
+      // pagination.total = data.totalCount;
+      this.setState({
+        data: data.results,
+        loading:false
+      });
+    });
+  }
+
+  componentDidMount() {
+    this.fetch();
+  }
   render() {
     return (
       <div className="App">
@@ -57,7 +85,13 @@ class App extends Component {
             </div>
           
 
-          <Table dataSource={dataSource} columns={columns} pagination={false} size="small"/>
+          <Table 
+          dataSource={this.state.data} 
+          columns={columns} 
+          pagination={false}
+          rowKey={(record) => record.matchId} 
+          loading={this.state.loading}
+          size="small"/>
           </div>
       </div>
     );
