2bacf8a5167d9cb9b07ab7803f589631160114ac
yyh123 <yunhaoyu@gmail.com>
2019-01-09 05:38:37 +0800

    数据模拟

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import Button from "antd/lib/button";
-import { Checkbox,Radio,Table  } from "antd";
+import { Checkbox,Radio,Table,Badge,Dropdown,Icon,Menu,message  } from "antd";
 import reqwest from 'reqwest';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
@@ -8,40 +8,13 @@ import './App.css';
 const RadioButton = Radio.Button;
 const RadioGroup = Radio.Group;
 
-const columns = [
-  {
-  title:'赛事',
-  dataIndex:'league[0]'
-  },
-  {
-  title:'时间',
-  dataIndex:'matchTime',
-  render:(value, record) => <span title={record.matchYear + '-' + record.matchDate + ' ' + record.matchTime}> {record.matchDate + ' ' + record.matchTime}</span>
-  },
-  {
-  title:'主队',
-  dataIndex:'home[0]'
-  },
-  {
-    title:'全场比分',
-    dataIndex:'score',
-    render:(value, record) => <span> {record.homeScore} - {record.guestScore} </span>
-  },
-  {
-    title:'客队',
-    dataIndex:'guest[0]'
-  },
-  {
-    title:'半场比分',
-    dataIndex:'halfScore',
-    render:(value, record) => <span> {record.homeHalfScore} - {record.guestHalfScore} </span>
-  },
-];
-
 class App extends Component {
   state = {
     data: [],
-    loading:false
+    loading:false,
+    lang:0,
+    showRed:true,
+    showYellow:true
   };
 
   fetch = () => {
@@ -64,7 +37,93 @@ class App extends Component {
   componentDidMount() {
     this.fetch();
   }
+  // 切换语言
+  handleLangChange = (e) => {
+    this.setState({
+      lang: e.target.value
+    })
+  }
+  // 显示红牌
+  handleShowRedChange = (e) => {
+    this.setState({showRed: e.target.checked})
+  }
+  // 显示黄牌
+  handleShowYellowChange = (e) => {
+    this.setState({showYellow: e.target.checked})
+  }
+  // 主队进球
+  handleOperation = (matchId) => {
+    // message.success('This is a prompt message for success, and it will disappear in 10 seconds', 10);
+    let index = this.state.data.findIndex(x => x.matchId === matchId);
+    if(index !==-1) {
+      let currentMatch = this.state.data[index];
+      let currentMatchHome = currentMatch.home[this.state.lang];
+      let currentMatchGuest = currentMatch.guest[this.state.lang];
+      currentMatch.homeScore++;
+      let msg = <span><b className="text-danger"> {currentMatchHome} {currentMatch.homeScore} </b>: {currentMatch.guestScore} {currentMatchGuest} </span>
+      message.success(msg,2);
+      this.setState({
+        data: this.state.data
+      })
+    }
+  }
   render() {
+    const columns = [
+      {
+      title:'赛事',
+      dataIndex:'league',
+      render:league => <span> {league[this.state.lang]} </span>
+      },
+      {
+      title:'时间',
+      dataIndex:'matchTime',
+      render:(value, record) => <span title={record.matchYear + '-' + record.matchDate + ' ' + record.matchTime}> {record.matchDate + ' ' + record.matchTime}</span>
+      },
+      {
+      title:'主队',
+      dataIndex:'home',
+      render:(home,record) =><div> 
+        <Badge className="mr-1" count={record.homeYellow} style={{display:this.state.showYellow?'block':'none' ,borderRadius:0, backgroundColor:'yellow',color:'#999',boxShadow:'0 0 0 1px #d9d9d9 inset'}} />
+        <Badge className="mr-1" count={record.homeRed} style={{display:this.state.showRed?'block':'none' ,borderRadius:0, backgroundColor:'red',color:'#fff',boxShadow:'0 0 0 1px #d9d9d9 inset'}} />
+        <span> {home[this.state.lang]} </span>
+        </div>
+      },
+      {
+        title:'全场比分',
+        dataIndex:'score',
+        render:(value, record) => <span> {record.homeScore} - {record.guestScore} </span>
+      },
+      {
+        title:'客队',
+        dataIndex:'guest',
+        render:(guest,record) =><div> 
+        <span> {guest[this.state.lang]} </span>
+        <Badge className="ml-1" count={record.guestRed} style={{display:this.state.showRed?'block':'none', borderRadius:0, backgroundColor:'red',color:'#fff',boxShadow:'0 0 0 1px #d9d9d9 inset'}} />
+        <Badge className="ml-1" count={record.guestYellow} style={{display:this.state.showYellow?'block':'none',borderRadius:0, backgroundColor:'yellow',color:'#999',boxShadow:'0 0 0 1px #d9d9d9 inset'}} />
+        </div>
+      },
+      {
+        title:'半场比分',
+        dataIndex:'halfScore',
+        render:(value, record) => <span> {record.homeHalfScore} - {record.guestHalfScore} </span>
+      },
+      {
+        title:'本地模拟',
+        dataIndex:'operation',
+        render:(value, record) => <Dropdown overlay={
+          <Menu>
+            <Menu.Item key="0">
+              <a onClick={e=>this.handleOperation(record.matchId)}>主队进球+1</a>
+            </Menu.Item>
+            <Menu.Divider />
+          </Menu>
+        } trigger={['click']}>
+        <a className="ant-dropdown-link">
+          模拟 <Icon type="down"/>
+        </a>
+      </Dropdown>,
+      },
+    ];
     return (
       <div className="App">
           
@@ -75,12 +134,12 @@ class App extends Component {
           </nav>
           <div className="container mt-3">
             <div className="filter my-3">
-              <Checkbox >显示红牌</Checkbox>
-              <Checkbox>显示黄牌</Checkbox>
-              <RadioGroup  defaultValue="0">
-                <RadioButton value="0">简体</RadioButton>
-                <RadioButton value="1">繁体</RadioButton>
-                <RadioButton value="2">English</RadioButton>
+              <Checkbox checked={this.state.showRed} onChange={this.handleShowRedChange}>显示红牌</Checkbox>
+              <Checkbox checked={this.state.showYellow} onChange={this.handleShowYellowChange} >显示黄牌</Checkbox>
+              <RadioGroup  defaultValue={this.state.lang} onChange={this.handleLangChange}>
+                <RadioButton value={0}>简体</RadioButton>
+                <RadioButton value={1}>繁体</RadioButton>
+                <RadioButton value={2}>English</RadioButton>
               </RadioGroup>
             </div>
           
